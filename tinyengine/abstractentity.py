647689d957602fa630e83bbf52c612ff6f173d2c
Eric Dalrymple <eric.dalrymple@gmail.com>
2025-03-25 00:57:35 -0400

    Added additional transform fields to entities.

--- tinyengine/abstractentity.py
+++ tinyengine/abstractentity.py
@@ -8,6 +8,8 @@ class AbstractEntity():
     
     # Properties
     position : Vector2 = Vector2(0, 0)
+    scale : Vector2 = Vector2(1, 1)
+    rotation : float = 0.0
     
     def __init__(self, startPosition : Vector2):
         self.position = startPosition
