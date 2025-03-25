f53cd41f36bd1bf438f1959fd679df4edba1d999
Eric Dalrymple <eric.dalrymple@gmail.com>
2025-03-25 09:27:58 -0400

    Excluding .doc directory from the package.

--- deploy.bat
+++ deploy.bat
@@ -12,7 +12,6 @@ if not exist "%packDir%" (
 )
 
 :: Copy the files we want to package
-xcopy "%rootDir%.doc\" "%packDir%.doc\" /E /Y
 xcopy "%rootDir%.vscode\" "%packDir%.vscode\" /E /Y
 xcopy "%rootDir%images\" "%packDir%images\" /E /Y
 xcopy "%rootDir%lessons\" "%packDir%lessons\" /E /Y
