5d544cab75e2d62c7e325403fd7e18af72c50673
Eric Dalrymple <eric.dalrymple@proton.me>
2025-07-13 12:37:41 -0400

    Separated out and improved packaging and deployment scripts.

--- deploy.bat
+++ deploy.bat
@@ -1,29 +1,22 @@
+@echo off
+
+setlocal
+
 set rootDir=%~dp0
 set packDir=%rootDir%.packaged\
 set deployDir=C:\Users\choco.EUCLID\Sync\Shared\Ghys\
-
 set packName=PyLab.zip
 
-:: Ensure .package directory exists and clean.
-if not exist "%packDir%" (
-    mkdir "%packDir%"
-) else (
-    del "%packDir%" /q
+set /p input_package="Would you like to re-package prior to deploying? (y/n)"
+if /i "%input_package%"=="y" (
+    call .\package.bat
 )
 
-:: Copy the files we want to package
-xcopy "%rootDir%.vscode\" "%packDir%.vscode\" /E /Y
-xcopy "%rootDir%game\" "%packDir%game\" /E /Y
-xcopy "%rootDir%images\" "%packDir%images\" /E /Y
-xcopy "%rootDir%lessons\" "%packDir%lessons\" /E /Y
-xcopy "%rootDir%tinyengine\" "%packDir%tinyengine\" /E /Y
-copy "%rootDir%__init__.py" "%packDir%__init__.py" /Y
-copy "%rootDir%requirements.txt" "%packDir%requirements.txt" /Y
-copy "%rootDir%setup.bat" "%packDir%setup.bat" /Y
-copy "%rootDir%update.bat" "%packDir%setup.bat" /Y
-
-:: Zip the package contents
-7z a -tzip "%rootDir%%packName%" "%packDir%*"
-
 :: Copy to deployment location
+echo Deploying to "%deployDir%"...
 copy "%rootDir%%packName%" "%deployDir%%packName%"
+
+echo Deployment complete!
+
+endlocal
+pause
---
+++ package.bat
@@ -0,0 +1,40 @@
+@echo off
+
+setlocal
+
+set rootDir=%~dp0
+set packDir=%rootDir%.packaged\
+
+set packName=PyLab.zip
+
+:: Ensure .package directory exists and clean.
+if not exist "%packDir%" (
+    mkdir "%packDir%"
+) else (
+    rmdir "%packDir%" /S /Q
+)
+
+:: Copy the files we want to package
+echo Staging files to "%packDir%"...
+set exclude_dirs=build_Windows __pycache__ >nul 2>&1
+robocopy "%rootDir%.vscode" "%packDir%.vscode" /E /XD %exclude_dirs% >nul 2>&1
+robocopy "%rootDir%game" "%packDir%game" /E /XD %exclude_dirs% >nul 2>&1
+robocopy "%rootDir%images" "%packDir%images" /E /XD %exclude_dirs% >nul 2>&1
+robocopy "%rootDir%lessons" "%packDir%lessons" /E /XD %exclude_dirs% >nul 2>&1
+robocopy "%rootDir%tinyengine" "%packDir%tinyengine" /E /XD %exclude_dirs% >nul 2>&1
+copy "%rootDir%requirements.txt" "%packDir%requirements.txt" /Y >nul 2>&1
+copy "%rootDir%setup.bat" "%packDir%setup.bat" /Y >nul 2>&1
+copy "%rootDir%update.bat" "%packDir%setup.bat" /Y >nul 2>&1
+
+:: Zip the package contents
+echo Packaging files to "%rootDir%%packName%"... 
+7z a -tzip "%rootDir%%packName%" "%packDir%*" >nul 2>&1
+
+:: Remove the packaging directory
+echo Cleaning up staging directory...
+rmdir "%packDir%" /S /Q
+
+echo Packaging complete!
+
+endlocal
+pause
