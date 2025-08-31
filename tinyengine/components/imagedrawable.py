cdfe7eccec3cf5c0449bd73e81f416aa8bae1b4a
Eric Dalrymple <eric.dalrymple@proton.me>
2025-08-31 14:48:45 -0400

    Added the ability to specify image sizes.

--- tinyengine/components/imagedrawable.py
+++ tinyengine/components/imagedrawable.py
@@ -12,20 +12,26 @@ class ImageDrawable(tinyengine.component.Component):
 
     _imagePath = ""
     _image = None
+    _width = 0.0
+    _height = 0.0
 
-    def __init__(self, image_path):
+    def __init__(self, image_path: str, width: float = 0.0, height: float = 0.0):
         self._imagePath = image_path
         self._image = pygame.image.load(self._imagePath)
+
+        self._width = width
+        self._height = height
+        self._image = pygame.transform.scale(self._image, (self._width, self._height))
 
 
     def draw(self, surface):
         pos : pygame.Vector2 = self.entity.transform.get_world_position()
-        width = self._image.get_rect().width
-        height = self._image.get_rect().height
         surface.blit(
             self._image,
-            (pos.x - (width >> 1),
-             pos.y - (height >> 1),
-             width,
-             height)
+            (
+                pos.x - (self._width >> 1),
+                pos.y - (self._height >> 1),
+                self._width,
+                self._height
+            )
         )
