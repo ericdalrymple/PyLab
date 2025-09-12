5f7923389138ab3e642338822c27a181166429f5
Eric Dalrymple <eric.dalrymple@proton.me>
2025-09-12 09:08:47 -0400

    Removed dead end experiments.

--- tiled/tilesets/house_details.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="house_details" tilewidth="16" tileheight="16" tilecount="170" columns="10">
- <image source="../tilemaps/house_details.png" width="160" height="272"/>
-</tileset>
